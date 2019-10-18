0fe95fa96dfdbcb6bbcb4073c361d26482e13eda
xdsoar <xdsoar@gmail.com>
2019-10-18 12:05:45 +0800

    add package setup file

---
+++ TaskCommander/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+requirements = []
+
+test_requirements = [
+    'pytest'
+]
+
+setup(name='TaskCommander', version='0.1', package=find_packages, install_requires=requirements,
+      test_requirements=test_requirements)
